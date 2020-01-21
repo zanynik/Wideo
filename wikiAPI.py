74ad0f400c251ca436a7e585630a148fa3c08144
zanynik <zanynik@gmail.com>
2020-01-22 00:49:36 +0100

    svg

--- wikiAPI.py
+++ wikiAPI.py
@@ -1,19 +1,13 @@
 import requests
 import urllib.request
+import urllib.parse
 import wikipedia
+import html
 import html2text
-from nltk.corpus import stopwords
-from nltk.corpus import wordnet
+# from nltk.corpus import stopwords
+# from nltk.corpus import wordnet
 from textblob import TextBlob
 import string
-
-def imgsave(page):
-    for words in page.split(" "):
-        if words in img:
-            if img[-3:] == "jpg":
-                urllib.request.urlretrieve(img, page + ".jpg")
-                print("Got image")
-                return True
 
 def firstImg(page):
     S = requests.Session()
@@ -31,29 +25,41 @@ def firstImg(page):
 
     for lines in DATA["parse"]["text"]["*"].split("File:",2)[1:]:
         if lines.split(".jpg",1)[1]:
+            type = "jpg"
             imgname = lines.split(".jpg",1)[0]
-            break
-    return imgname
+            return urllib.parse.quote_plus(imgname), type
+        if lines.split(".svg",1)[1]:
+            type = "svg"
+            imgname = lines.split(".svg",1)[0]
+            return urllib.parse.quote_plus(imgname), type
+        #Need to Encode the names
 
-
-wikiPage = wikipedia.page("Albert Einstein")
+wikiPage = wikipedia.page("Elinor Ostrom")
 
 text = wikiPage.content.split("references",1)[0]
 content = text.translate(str.maketrans('', '', string.punctuation))
 blob = TextBlob(content)
 noun_set = set(blob.noun_phrases)
-
+refpages =[]
 for noun in noun_set:
+    pageset = set(refpages)
     try:
         searchPage = wikipedia.search(noun)[0]
-        name = firstImg(searchPage)
+        name,ext = firstImg(searchPage)
         print("img name ",name)
-        for img in wikipedia.page(searchPage).images:
-            print("img : ", img)
-            if name in img:
-                urllib.request.urlretrieve(img, searchPage + ".jpg")
-                print("Got First image")
-                break
+        if name not in pageset:
+            for img in wikipedia.page(searchPage).images:
+                print("img : ", img)
+                if name in img:
+                    if ext == "jpg":
+                        urllib.request.urlretrieve(img, "Elinor/"+ searchPage + ".jpg")
+                        print("Got JPG image")
+                        break
+                    if ext == "svg":
+                        urllib.request.urlretrieve(img, "Elinor/"+ searchPage + ".svg")
+                        print("Got SVG image")
+                        break
+        refpages.append(name)
 
     except Exception:
         pass
@@ -63,6 +69,6 @@ for link in wikiPage.links:
     imgname = firstImg(link)
     for img in wikipedia.page(link).images:
         if imgname in img:
-            urllib.request.urlretrieve(img, link + ".jpg")
+            urllib.request.urlretrieve(img, "Elinor/"+link + ".jpg")
             print("Got Link image")
             break
